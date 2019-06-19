b6d2281b2c5f7aa667270a09841f3ad7bfc733f3
jacobzielinski <32741770+jacobzielinski@users.noreply.github.com>
2019-06-19 15:06:54 +0100

    first commit

--- app.js
+++ app.js
@@ -1,54 +1,43 @@
 let mobilenet;
-let penguin;
-let dropzone;
-let display = document.getElementById("data")
-let display2 = document.getElementById("data2")
-let display3 = document.getElementById("data3")
-let visibility = document.getElementById("middleinfo")
+let video;
+let label = ""
+let toClear;
 
 
-function  modelReady(file){
-   let img = createImg(file.data)
-
-   console.log("Model is ready!")
-   mobilenet.predict(img, gotResults)
-} 
+function modelReady(){
+   console.log("model is Ready!")
+   mobilenet.predict(gotResults)
+}
 
 function gotResults(error, results){
-   if (error) {
-      console.error(error);
-   } else {
-      let text = Math.floor(results[0].confidence * 100) + "% confidence"
+   if(error) {
+      console.error(error)
+   }else{
       console.log(results)
-      console.log(results[0].label + " " + Math.floor(results[0].confidence * 100) + "%")
-      console.log(results[0].label + " " + results[0].confidence + "%")
-      display.innerHTML = results[0].label + " " + text.bold();
-      display2.innerHTML = results[1].label + " " + Math.floor(results[1].confidence * 100) + "%"
-      display3.innerHTML = results[2].label + " " + Math.floor(results[2].confidence * 100) + "%"
-      textSize(36)
-      visibility.style.visibility = "visible"
-
-      
-      
+      label = results[0].label;
+      mobilenet.predict(gotResults)
+      toClear.clear()
    }
+
 }
 
-function setup() {
-   mobilenet = ml5.imageClassifier("MobileNet");
-   dropzone = select('#dropzone')
-   dropzone.dragOver(highlight)
-   dropzone.dragLeave(unhighlight)
-   dropzone.drop(modelReady, unhighlight)
+// function imageReady() {
+//    image(puffin, 0, 0, width, height);
+// }
+
+function  setup () {
+   createCanvas(640, 550);
+   video = createCapture(VIDEO)
+   video.hide()
+   background(0);
+   mobilenet = ml5.imageClassifier('MobileNet', video, modelReady)
 }
 
-function highlight() {
-   dropzone.style('background-color', '#7395a0')
-   
-}
-
-function unhighlight() {
-   dropzone.style('background-color', '#9dccdb')
-
+function draw(){
+image(video, 0, 0)
+fill(0);
+textSize(32)
+toClear = text(label, 10, height - 20)
 }
 
 
