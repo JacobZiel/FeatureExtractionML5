6f68198f0f39dbdfccf6254a90d401d8babc9489
jacobzielinski <32741770+jacobzielinski@users.noreply.github.com>
2019-06-21 12:16:10 +0100

    Hello

--- app.js
+++ app.js
@@ -1,39 +1,84 @@
+//#region var
 let mobilenet;
 let video;
 let label = ""
 let toClear;
+let classifier;
+let addButtonUp;
+let addButtonDown;
+let neutralButton;
+let trainButton;
+//#endregion
 
+let firstImage = "Connor"
+let secondImage = "Bottle of Drink"
+let neautral = "Wall"
+
+//#region classifer
 function modelReady(){
    console.log("model is Ready!")
-   mobilenet.predict(gotResults)
+   
+}
+
+function videoReady(){
+   console.log("video is Ready!")
+}
+
+function whileTraining(loss) {
+   if(loss == null){
+   console.log("Training Complete");
+   classifier.classify(gotResults);
+   }else{
+      console.log(loss);
+   }
 }
 
 function gotResults(error, results){
    if(error) {
       console.error(error)
+   // } else if (results[0].label !== firstImage && results[0].label !== secondImage) {
+   //       document.getElementById("test").innerHTML = "Not recognized"
+   //    }else{
    }else{
       console.log(results)
-      label = results[0].label;
-      mobilenet.predict(gotResults)
-      toClear.clear()
+      // label = results[0].label
+      document.getElementById("test").innerHTML = results[0].label + " " + Math.floor(results[0].confidence * 100) + "%";
+      classifier.classify(gotResults)
+      }
+      // toClear.clear()
    }
-}
+
 
 
 function  setup () {
-   createCanvas(640, 550);
+   // createCanvas(640, 550);
    video = createCapture(VIDEO)
-   video.hide()
+   // video.hide()
    background(0);
-   mobilenet = ml5.imageClassifier('MobileNet', video, modelReady)
+   mobilenet = ml5.featureExtractor('MobileNet', modelReady)
+   classifier = mobilenet.classification(video, videoReady)
+//#endregion
+   addButtonUp = createButton('Face');
+   addButtonUp.mousePressed(function(){
+      classifier.addImage(firstImage)
+   })
+
+   addButtonDown = createButton('Item');
+   addButtonDown.mousePressed(function () {
+      classifier.addImage(secondImage)
+   })
+
+   neutralButton = createButton('Neutral');
+   neutralButton.mousePressed(function () {
+      classifier.addImage(neautral)
+   })
+
+   trainButton = createButton('TRAIN');
+   trainButton.mousePressed(function () {
+      classifier.train(whileTraining);
+   })
 }
 
-function draw(){ 
-image(video, 0, 0,)
-fill(0);
-textSize(32)
-toClear = text(label, 70, height - 20)
-}
 
 
 
